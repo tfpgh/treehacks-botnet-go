0456deccb211a8ccd0a98ab661b0565ed49ba9ba
Toby Penner <tobypenner01@gmail.com>
2023-11-26 20:44:48 -0700

    Added bot name generation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tfpgh/treehacks-botnet-go
 
 go 1.21.4
+
+require github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	botName, err := generateBotName()
+	if err != nil {
+		fmt.Printf("Failed to generate unique bot name, %v. Using \"%v\"\n", err, botName)
+	} else {
+		fmt.Printf("Generated unique bot name: \"%v\"\n", botName)
+	}
+}
+
+func generateBotName() (string, error) {
+	id, err := gonanoid.New()
+	if err != nil {
+		// Failed to generate unique ID
+		return "default", err
+	}
+
+	username := os.Getenv("USER")
+	if len(username) > 0 {
+		// Username was found. Returning username + id
+		return fmt.Sprintf("%v-%v", username, id), nil
+	} else {
+		// No username. Just returning id
+		return id, nil
+	}
+
 }
